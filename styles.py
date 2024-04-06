4d400ded1dff445ec9697cb182f5305d216e537c
Arun Paul Varghese <arunjeevan98@gmail.com>
2024-04-06 00:56:00 -0400

    added another code

--- styles.py
+++ styles.py
@@ -13,3 +13,19 @@ def indexOfFirst(needle):
 
 n = 'a,b,s,a,f,a'
 print(indexOfFirst(n))
+
+
+#Search Insert position using binary search and O(logn)
+
+def insertSearch(num, target):
+    l = 0
+    r = len(num) - 1
+    while l <= r:
+        mid = (r+l) // 2
+        if num[mid] < target:
+            l = mid + 1
+        elif num[mid] > target:
+            r = mid - 1
+        else:
+            return mid
+    return l
