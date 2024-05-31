0dc6faa0fb715095a812cbc8891a95bf6a422de6
Arun Paul Varghese <arunjeevan98@gmail.com>
2024-05-31 00:32:11 -0400

    updated merging two list code

--- merge_sorted_list.py
+++ merge_sorted_list.py
@@ -1,13 +1,22 @@
-class ListNode:
-    def __init__(self, val=0, next=None):
+
+class ListNode():
+    def __init__(self, val = 0,next = None):
         self.val = val
         self.next = next
 
-class Solution(object):
-    def mergeTwoLists(self, list1, list2):
+
+def list_to_linkedlist(elements):
+    head = ListNode(elements[0])
+    current = head
+    for numbers in elements:
+        current.next = ListNode(numbers)
+        current = current.next
+    return head
+
+class SortList():
+    def merge_sorted_list(self,list1,list2):
         head = ListNode()
         current = head
-
         while list1 and list2:
             if list1.val < list2.val:
                 current.next = list1
@@ -20,27 +29,19 @@ class Solution(object):
             current.next = list1
         else:
             current.next = list2
-        return head.next
+        return head
 
-def list_to_linkedlist(elements):
-    head = ListNode(elements[0])
-    current = head
-    for element in elements[1:]:
-        current.next = ListNode(element)
-        current = current.next
-    return head
+def linkedlist_to_list(merge):
+    list = []
+    while merge:
+        list.append(merge.val)
+        merge = merge.next
+    return list
 
-def linkedlist_to_list(node):
-    elements = []
-    while node:
-        elements.append(node.val)
-        node = node.next
-    return elements
 
 l1 = list_to_linkedlist([2, 5, 8, 9])
 l2 = list_to_linkedlist([1, 2, 4, 5, 7])
+sort = SortList()
+merge = sort.merge_sorted_list(l1,l2)
+print(linkedlist_to_list(merge))
 
-solution = Solution()
-merged_head = solution.mergeTwoLists(l1, l2)
-merged_list = linkedlist_to_list(merged_head)
-print(merged_list)
